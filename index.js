188b738a2b1c0bc59d25850e4a7de721699bfea9
Julia <jlchen6@wisc.edu>
2020-03-26 17:36:03 -0500

    Got API call working and logging data to console

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+// import the required libraries
 var fs = require("fs");
 var inquire = require("inquirer");
-var api = require("./util/api.js")
+var api = require("./util/api.js");
 
+// Array of questions to feed into inquirer
 const questions = [{
     message: "Enter your GitHub username: ",
     name: "username"
+},
+{
+    message: "What is the repo name that you would like to generate a README for?: ",
+    name: "project"
 }];
 
+// Function that will write the given data into a file.
 function writeToFile(fileName, data) {
     
 }
 
+// Main function that initializes the program
 function init() {
-    inquire.prompt(questions).then(function({username}){
-        api.getUser(username);
+    // Ask user for their username and which repo they want to create a README for.
+    inquire.prompt(questions).then(function({username, project}){
+        // Call the function to get the user data from the api
+        var userData = api.getUser(username);
+        // Then, after the api call is done, pass the userdata to the markdown function that stores that data in a string
+
+        // Pass the created string to the function that writes the README file.
     })
 
 }
--- util/api.js
+++ util/api.js
@@ -1,14 +1,18 @@
+// import axios to make the API call
 const axios = require("axios");
 
+// Create an export variable with the API call
 const api = {
+  // Function to call the Github API
   getUser(username) {
-    var queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`
+    var queryUrl = `https://api.github.com/users/${username}`;
 
     axios.get(queryUrl).then(function(res){
-      
+      console.log(res.data);
+      return res.data;
+    }).catch(function(error){
+      throw error;
     });
-    
-
   }
 };
 
