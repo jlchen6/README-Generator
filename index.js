3b28eb8412e29746b2b7963be8f5e1bdef0487ba
Julia <jlchen6@wisc.edu>
2020-03-26 11:24:39 -0500

    Added prompt for username, laid out foundation for axios call

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const questions = [
+var fs = require("fs");
+var inquire = require("inquirer");
+var api = require("./util/api.js")
 
-];
+const questions = [{
+    message: "Enter your GitHub username: ",
+    name: "username"
+}];
 
 function writeToFile(fileName, data) {
+    
 }
 
 function init() {
+    inquire.prompt(questions).then(function({username}){
+        api.getUser(username);
+    })
 
 }
 
--- util/api.js
+++ util/api.js
@@ -1,5 +1,13 @@
+const axios = require("axios");
+
 const api = {
   getUser(username) {
+    var queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`
+
+    axios.get(queryUrl).then(function(res){
+      
+    });
+    
 
   }
 };
