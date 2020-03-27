d83298ffb6fb63c158a1b0b5ed6c135cc020d088
Julia <jlchen6@wisc.edu>
2020-03-26 21:19:35 -0500

    Tried chaining promises to perform asynchronous functions sequentially

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
 // import the required libraries
-var fs = require("fs");
-var inquire = require("inquirer");
-var api = require("./util/api.js");
+const fs = require("fs");
+const inquire = require("inquirer");
+const axios = require("axios");
+// const api = require("./util/api.js");
+const genMark = require("./util/generateMarkdown.js")
+const util = require("util")
+var title = "";
+
+// Convert functions into promise based functions so can call them in order later
+// const getUserAsync = util.promisify(api.getUser);
+const genMarkAsync = util.promisify(genMark);
+const writeFileAsync = util.promisify(writeToFile);
+
+const config = { headers: { accept: "application/json" } };
 
 // Array of questions to feed into inquirer
 const questions = [{
@@ -15,20 +27,47 @@ const questions = [{
 
 // Function that will write the given data into a file.
 function writeToFile(fileName, data) {
-    
+    console.log("Writing file.")
 }
+
+function getUser(username) {
+    var queryUrl = `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
+
+    axios.get(queryUrl, config)
+    .then(function(res) {
+      console.log(res.data);
+      // Then, after the api call is done, pass the userdata to the markdown function that stores that data in a string
+      return genMarkAsync(res.data);
+    })
+    // Pass the created string to the function that writes the README file.
+    .then(function (fileData) {
+        console.log("reached genMark");
+        return writeFileAsync("README.md", fileData);
+    })
+    .then(function () {
+       console.log("Finished writing README!");
+    })
+    .catch(function(error){
+      throw error;
+    });
+  }
 
 // Main function that initializes the program
 function init() {
+
+    var askQuestions = inquire.prompt(questions);
+
     // Ask user for their username and which repo they want to create a README for.
-    inquire.prompt(questions).then(function({username, project}){
-        // Call the function to get the user data from the api
-        var userData = api.getUser(username);
-        // Then, after the api call is done, pass the userdata to the markdown function that stores that data in a string
-
-        // Pass the created string to the function that writes the README file.
-    })
-
+    askQuestions
+        .then(function (response) {
+            // Call the function to get the user data from the api and return it as part of the promise
+            console.log("getting user data");
+            title = response.project;
+            getUser(response.username);
+        })
+        .catch(function (error) {
+            throw error;
+        })
 }
 
 init();
--- util/api.js
+++ util/api.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const api = {
   // Function to call the Github API
   getUser(username) {
-    var queryUrl = `https://api.github.com/users/${username}`;
+    var queryUrl = `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
 
     axios.get(queryUrl).then(function(res){
       console.log(res.data);
